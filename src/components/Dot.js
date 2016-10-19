9d2f34af092f04c4ca216c5f6f9967fb28736b02
wu0792 <wu0792@163.com>
2016-10-19 08:45:01 +0800

    modify the dot event handler.

--- src/components/Dot.js
+++ src/components/Dot.js
@@ -7,16 +7,16 @@ class Dot extends React.Component{
     this.state = {};
     this.state.seq = props.seq || 0;
     this.state.activeSeq = props.activeSeq || 0;
-    this.onClick = this.onClick.bind(this); 
+    // this.onClick = this.onClick.bind(this); 
   }
 
   render(){
-    return (<li className={'nav_dot' + (this.state.activeSeq == this.state.seq?' active':'')} onClick={this.onClick}></li>)
+    return (<li className={'nav_dot' + (this.state.activeSeq == this.state.seq?' active':'')}></li>)
   }
 
-  onClick(e){
-    this.setState({activeSeq: this.state.seq});
-  }
+  // onClick(e){
+  //   this.setState({activeSeq: this.state.seq});
+  // }
 }
 
 export default Dot;
